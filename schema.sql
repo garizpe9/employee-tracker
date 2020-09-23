4b0489513baf906161257fc807f677e3d70aa6b6
Giovanna Arizpe <veliaarizpe@gmail.com>
2020-09-23 14:45:48 -0500

    restarted table and added foreign keys

--- schema.sql
+++ schema.sql
@@ -3,29 +3,65 @@ CREATE database employee_tracker;
 
 USE employee_tracker;
 
-CREATE TABLE department (
-  id INT AUTO_INCREMENT NOT NULL,
-  name VARCHAR(30) NULL,
-  PRIMARY KEY (id)
+CREATE TABLE department
+(
+  id INT NOT NULL
+  AUTO_INCREMENT,
+  name VARCHAR
+  (30) NULL,
+  PRIMARY KEY
+  (id)
+
 );
 
-CREATE TABLE role (
-  id INT AUTO_INCREMENT NOT NULL,
-  title VARCHAR(100) NOT NULL,
-  salary DECIMAL(65) NOT NULL,
+  CREATE TABLE role
+  (
+    id INT NOT NULL
+    AUTO_INCREMENT,
+  title VARCHAR
+    (30) NOT NULL,
+  salary DECIMAL
+    (65) NOT NULL,
   department_id INT NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY
+    (id),
+  FOREIGN KEY
+    (department_id)
+  REFERENCES department
+    (id)
+  ON
+    DELETE CASCADE
 );
 
-CREATE TABLE employee (
-  id INT AUTO_INCREMENT NOT NULL,
-  first_name VARCHAR(100) NOT NULL,
-  last_name VARCHAR(100) NOT NULL,
-  role_id VARCHAR(100) NOT NULL,
-  manager_id VARCHAR(100) NOT NULL,
-  PRIMARY KEY (id)
+    CREATE TABLE employee
+    (
+      id INT NOT NULL
+      AUTO_INCREMENT,
+  first_name VARCHAR
+      (30) NOT NULL,
+  last_name VARCHAR
+      (30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY
+      (id),
+  FOREIGN KEY
+      (role_id)
+  REFERENCES role
+      (id)
+  ON
+      DELETE CASCADE,
+  FOREIGN KEY
+      (manager_id)
+      REFERENCES employee
+      (id)
+  ON
+      DELETE CASCADE
 );
 
-SELECT * FROM department;
-select * from role;
-select * from employee;
+      SELECT *
+      FROM department;
+      select *
+      from role;
+      select *
+      from employee;
